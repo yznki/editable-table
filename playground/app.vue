eeb57f449aa03236aa4ce6dc3583ac10fc54138d
yznki <yznkiswani@gmail.com>
2025-12-17 11:33:19 +0300

    feat: integrate active-table component and update playground example

--- playground/app.vue
+++ playground/app.vue
@@ -1,3 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const data = ref([]);
+  const columns = 25;
+  const rows = 200;
 
-<template></template>
+  function fillUpArray() {
+    const header = Array.from({ length: columns }, (_, colIdx) => `col${colIdx + 1}`);
+    const rowsData = Array.from({ length: rows }, () => Array.from({ length: columns }, () => "test"));
+    return [header, ...rowsData];
+  }
+
+  onMounted(() => {
+    data.value = fillUpArray() as never[];
+  });
+</script>
+
+<template>
+  <EditableTable v-model="data" />
+
+  <pre>{{ data }}</pre>
+</template>
---
+++ src/runtime/components/EditableTable.client.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import "active-table";
+
+  const data = defineModel({ default: [] });
+</script>
+
+<template>
+  <active-table :data="data" />
+</template>
